b35e7feaf9da95c0453731ba14015535dbe50979
Fredrik Jacobsen <fredrik@MacBookPro.lan>
2025-04-15 12:02:09 +0200

    ✅ Fikset case-sensitiv import for Vercel build

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
+import './index.css';
 import React from "react";
 import ReactDOM from "react-dom/client";
-import LagGenerator from "./Laggenerator";
-import "./index.css";
+import LagGenerator from "./Laggenerator.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
