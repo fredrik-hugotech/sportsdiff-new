3ee966165d7da2ec54a319496c714d782ad73103
Fredrik Jacobsen <fredrik@MacBookPro.lan>
2025-04-15 12:34:00 +0200

    🔧 Bruker TeamGenerator som main komponent

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import LagGenerator from "./Laggenerator.jsx"; // merk stor G
+import LagGenerator from "./TeamGenerator.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
